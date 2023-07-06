2757181fa9fdea561cac39d37b955ec4e38fbd2c
pavelyanu <pyanushonak@gmail.com>
2023-07-06 14:36:11 +0000

    Added abstract agent class. Closes #5

---
+++ src/abstract_agent.py
@@ -0,0 +1,31 @@
+import abc
+
+class AbstractAgent(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, observation_space, action_space):
+        self._observation_space = observation_space
+        self._action_space = action_space
+
+    @property
+    def observation_space(self):
+        return self._observation_space
+
+    @property
+    def action_space(self):
+        return self._action_space
+
+    @abc.abstractmethod
+    def act(self, state):
+        pass
+
+    @abc.abstractmethod
+    def learn(self, state, action, reward, next_state, done):
+        pass
+
+    @abc.abstractmethod
+    def save(self, path):
+        pass
+
+    @abc.abstractmethod
+    def load(self, path):
+        pass
