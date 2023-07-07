cc2ee1f83283e6a514ae198befd7163decf43c04
pavelyanu <pyanushonak@gmail.com>
2023-07-07 11:59:57 +0200

    Added download_data.sh script.

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 # Default download directory
 DOWNLOAD_DIR="./dataset"
@@ -11,13 +11,13 @@ DRY_RUN=0
 
 while getopts "d:v:n:" opt; do
   case ${opt} in
-    -d | --directory )
+    d )
       DOWNLOAD_DIR=$OPTARG
       ;;
-    -v | --verbose )
+    v )
       VERBOSE=$OPTARG
       ;;
-    -n | --dry_run )
+    n )
       DRY_RUN=$OPTARG
       ;;
     \? )
@@ -29,27 +29,22 @@ shift $((OPTIND -1))
 
 # Define your download links
 declare -A LINKS=( 
-  # ["nld-aa.zip"]=""
-  # ["nld-nao_part1.zip"]=""
-  # ["nld-nao_part2.zip"]=""
-  # ["nld-nao_part3.zip"]=""
-  # ["nld-nao_part4.zip"]=""
-  # ["nld-nao_xlogfiles.zip"]=""
+  ["nld-aa.zip"]="https://drive.google.com/uc?id=1AKLA9ektEYwmIID5w_D7hLj2Kt4Sd0Xi&export=download"
 )
 
 # Download and unzip
 for file in "${!LINKS[@]}"; do
   link=${LINKS[$file]}
   if [ $VERBOSE -eq 1 ] || [ $DRY_RUN -eq 1 ]; then
-    echo "Would download $file from $link"
-    echo "Would unzip $file"
+	echo "Would download $file from $link"
+	echo "Would unzip $file"
   fi
   if [ $DRY_RUN -eq 0 ]; then
-    wget -P $DOWNLOAD_DIR $link
-    if [[ $file == "nld-aa.zip" ]]; then
-      unzip $DOWNLOAD_DIR/$file -d $DOWNLOAD_DIR/nld-aa
-    else
-      unzip $DOWNLOAD_DIR/$file -d $DOWNLOAD_DIR/nld-nao
-    fi
+	wget -P $DOWNLOAD_DIR $link -O $DOWNLOAD_DIR/$file
+	if [[ $file == "nld-aa.zip" ]]; then
+	  unzip $DOWNLOAD_DIR/$file -d $DOWNLOAD_DIR/nld-aa
+	else
+	  unzip $DOWNLOAD_DIR/$file -d $DOWNLOAD_DIR/nld-nao
+	fi
   fi
 done
