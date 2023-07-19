f2d403308d03435f3f6c864f72aae734cc3a9850
pavelyanu <pyanushonak@gmail.com>
2023-07-19 19:02:31 +0000

    added tensordict wrapper

--- src/utils/wrapper.py
+++ src/utils/wrapper.py
@@ -1,5 +1,6 @@
 import gym
 import torch
+from tensordict import TensorDict
 
 class MinihackWrapper(gym.Wrapper):
     def __init__(self, env):
@@ -17,3 +18,24 @@ class MinihackWrapper(gym.Wrapper):
 
     def _process_observation(self, observation):
         return observation['glyphs']
+
+class MinihackTensorDictWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        observation = self._process_observation(observation)
+        return observation, reward, done, info
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        observation = self._process_observation(observation)
+        return observation
+
+    def _process_observation(self, observation):
+        dictionary = {}
+        for key in observation.keys():
+            dictionary[key] = torch.tensor(observation[key]).unsqueeze(0)
+        ret = TensorDict(dictionary, batch_size=[1])
+        return ret
