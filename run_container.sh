9312f0faf143df221c62cd3a8bf904063dd2f898
pavelyanu <pyanushonak@gmail.com>
2023-08-01 17:30:37 +0000

    Added a new script to run the podman container.
    - sets gpu
    - adds current directory to python path
    - mounts the current directory to the container

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,7 @@
-podman run --device nvidia.com/gpu=0 -it -u 0 docker.io/pavelyanu/yanushonak /bin/bash
+podman run \
+	--device nvidia.com/gpu=0 \
+	--security-opt label=disable \
+    --security-opt seccomp=unconfined \
+	--env "PYTHONPATH=/workspaces" \
+    --volume "$(pwd):/workspaces" --workdir "/workspaces" \
+	-it -u 0 docker.io/pavelyanu/yanushonak /bin/bash
