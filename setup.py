454b393dedd6cd267d3499a3e130a88406472d26
pavelyanu <pyanushonak@gmail.com>
2023-08-03 20:51:51 +0000

    Minor changes to project packaging

--- setup.py
+++ setup.py
@@ -6,21 +6,20 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
-        'gym',
-        'minihack',
-        'nle',
-        'numpy',
-        'torch',
-        'tqdm',
-        'pandas',
-        'matplotlib',
-        'plotext'
+        'click==8.1.6',
+        'gym==0.23.0',
+        'minihack==0.1.5',
+        'nle==0.9.0',
+        'numpy==1.25.2',
+        'torch==2.0.1',
+        'tqdm==4.65.0',
+        'pandas==2.0.3',
+        'matplotlib==3.7.2',
+        'plotext==5.2.8'
     ],
     entry_points={
         'console_scripts': [
             'nethack_neural=nethack_neural.main:cli',
         ],
     }
-
 )
