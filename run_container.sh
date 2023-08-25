c117a364060de70fa313fcac4cf3589e5420d424
pavelyanu <pyanushonak@gmail.com>
2023-08-25 12:13:35 +0200

    Small changes to containers

--- run_container.sh
+++ run_container.sh
@@ -4,4 +4,4 @@ podman run \
     --security-opt seccomp=unconfined \
 	--env "PYTHONPATH=/workspaces" \
     --volume "$(pwd):/workspaces" --workdir "/workspaces" \
-	-it -u 0 docker.io/pavelyanu/yanushonak /bin/bash
+	-it -u 0 docker.io/pavelyanu/nethack_neural:dev /bin/bash
