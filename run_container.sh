b7cf502fba139ee6886f75c18e9ff095384423ef
pavelyanu <pyanushonak@gmail.com>
2024-04-27 10:44:12 +0200

    Removed old version of run_container

--- run_container.sh
+++
@@ -1,7 +0,0 @@
-podman run \
-	--device nvidia.com/gpu=0 \
-	--security-opt label=disable \
-    --security-opt seccomp=unconfined \
-	--env "PYTHONPATH=/workspaces" \
-    --volume "$(pwd):/workspaces" --workdir "/workspaces" \
-	-it -u 0 docker.io/pavelyanu/nethack_neural:dev /bin/bash
